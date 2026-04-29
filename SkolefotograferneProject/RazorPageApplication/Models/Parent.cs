3dac0f97fac87d39a0769ba1fe22e1ac35b0debe
Frederik Kristensen <frede.h.k1@gmail.com>
2026-04-29 13:00:37 +0200

    Implement Parent and Photo properties

--- SkolefotograferneProject/RazorPageApplication/Models/Parent.cs
+++ SkolefotograferneProject/RazorPageApplication/Models/Parent.cs
@@ -1,6 +1,36 @@
-﻿namespace RazorPageApplication.Models
+﻿using RazorPageApplication.Interfaces;
+
+namespace RazorPageApplication.Models
 {
-    public class Parent
+    public class Parent : IUser
     {
+        public int Id { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+
+        public Parent()
+        {
+            
+        }
+
+        public Parent(int id, string mail, string password, string name, string phoneNumber, string address, string postalCode)
+        {
+            Id = id;
+            Mail = mail;
+            Password = password;
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            PostalCode = postalCode;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
--- SkolefotograferneProject/RazorPageApplication/Models/Photo.cs
+++ SkolefotograferneProject/RazorPageApplication/Models/Photo.cs
@@ -2,5 +2,22 @@
 {
     public class Photo
     {
+        public int Id { get; set; }
+        public string FilePath { get; set; }
+        public DateTime Date { get; set; }
+        public Photographer Photographer { get; set; }
+        public Student Student { get; set; }
+
+        public Photo(int id, string filePath, DateTime date)
+        {
+            Id = id;
+            FilePath = filePath;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\n\tFilePath: {FilePath}\n\tDate: {Date}";
+        }
     }
 }
