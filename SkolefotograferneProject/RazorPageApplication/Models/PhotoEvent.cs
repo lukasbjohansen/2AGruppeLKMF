9024828934746159b1d3d2daafe6da7656397ec9
KasperKronmannMoeller <kam002@edu.zealand.dk>
2026-04-29 12:37:00 +0200

    Photoevent and Phtographer

--- SkolefotograferneProject/RazorPageApplication/Models/PhotoEvent.cs
+++ SkolefotograferneProject/RazorPageApplication/Models/PhotoEvent.cs
@@ -1,6 +1,40 @@
-﻿namespace RazorPageApplication.Models
+﻿using System.Runtime.ConstrainedExecution;
+using System.Xml.Linq;
+
+namespace RazorPageApplication.Models
 {
     public class PhotoEvent
     {
+        #region Properties
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Location { get; set; }
+        public Photographer Photographer { get; set; }
+        public SchoolClass SchoolClass { get; set; } 
+        #endregion
+
+        #region Constructors
+        public PhotoEvent()
+        {
+
+        }
+        public PhotoEvent(int id, DateTime startTime, DateTime endTime, string location, Photographer photographer, SchoolClass schoolClass)
+        {
+            Id = id;
+            StartTime = startTime;
+            EndTime = endTime;
+            Location = location;
+            Photographer = photographer;
+            SchoolClass = schoolClass;
+        } 
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"PhotoEvent: \n\tID: {Id}\n\tStartTime: {StartTime}\n\tEndTime: {EndTime}\n\tLocation: {Location}\n\tPhotographer: {Photographer}\n\tSchoolClass: {SchoolClass}";
+        } 
+        #endregion
     }
 }
--- SkolefotograferneProject/RazorPageApplication/Models/Photographer.cs
+++ SkolefotograferneProject/RazorPageApplication/Models/Photographer.cs
@@ -1,6 +1,43 @@
-﻿namespace RazorPageApplication.Models
+﻿using RazorPageApplication.Interfaces;
+
+namespace RazorPageApplication.Models
 {
-    public class Photographer
+    public class Photographer:IUser
     {
+        #region Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CVR { get; set; }
+        #endregion
+        #region Constructors
+        public Photographer()
+        {
+
+        }
+        public Photographer(int id,string name, string mail, string password, string phoneNumber, string cvr)
+        {
+            Id = id;
+            Name = name;
+            Mail = mail;
+            Password = password;
+            PhoneNumber = phoneNumber;
+            CVR = cvr;
+        } 
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"Photographer: \n\tID: {Id}\n\tName: {Name}\n\tMail: {Mail}\n\tPhoneNumber: {PhoneNumber}\n\tCVR: {CVR}";
+        } 
+        #endregion
+
+
+
+
+
     }
 }
