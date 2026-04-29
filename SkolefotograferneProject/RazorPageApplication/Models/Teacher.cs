024d32ca940bc261d960f9ebcaf55c0355389254
lukasbjohansen <lukasbjohansen@gmail.com>
2026-04-29 11:40:22 +0200

    documented teacher

--- SkolefotograferneProject/RazorPageApplication/Models/Teacher.cs
+++ SkolefotograferneProject/RazorPageApplication/Models/Teacher.cs
@@ -4,32 +4,47 @@ namespace RazorPageApplication.Models
 {
     public class Teacher : IUser
     {
-        public int Id { get; set; }
+		#region Properties
+        /// <summary>
+        /// Unique id. Primarily used by the database. Generated using identity.
+        /// </summary>
+		public int Id { get; set; }
+        /// <summary>
+        /// Email address. Must be unique.
+        /// </summary>
         public string Mail { get; set; }
+        /// <summary>
+        /// Password. Stored raw for this prototype. Ideally stored as a hash value.
+        /// </summary>
         public string Password { get; set; }
+        /// <summary>
+        /// Full name.
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Phonenumber. Must be unique.
+        /// </summary>
         public string PhoneNumber { get; set; }
-        public SchoolClass SchoolClass { get; set; }
-
-        public Teacher()
+		#endregion
+		#region Constructors
+		public Teacher()
         {
             
         }
-        public Teacher(int id, string mail, string password, string name, string phoneNumber, SchoolClass schoolClass)
+        public Teacher(int id, string mail, string password, string name, string phoneNumber)
         {
             Id = id;
             Mail = mail;
             Password = password;
             Name = name;
             PhoneNumber = phoneNumber;
-            SchoolClass = schoolClass;
         }
-
+        #endregion
+        #region Methods
         public override string ToString()
         {
             return $"Teacher:\n\tId: {Id}\n\tMail: {Mail}\n\tName: {Name}\n\tPhonenumber: {PhoneNumber}\n\tSchoolclass: {SchoolClass}";
-        }
+        } 
+        #endregion
     }
-
-    
 }
