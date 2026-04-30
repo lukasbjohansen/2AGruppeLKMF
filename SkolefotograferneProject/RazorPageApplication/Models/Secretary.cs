9dd3954a3cf6f43078409538243e8abe8afe828c
KasperKronmannMoeller <kam002@edu.zealand.dk>
2026-04-30 09:07:35 +0200

    Added regions for School,SchoolClass, Secretary and Student classes

--- SkolefotograferneProject/RazorPageApplication/Models/Secretary.cs
+++ SkolefotograferneProject/RazorPageApplication/Models/Secretary.cs
@@ -4,13 +4,16 @@ namespace RazorPageApplication.Models
 {
     public class Secretary : IUser
     {
+        #region Properties
         public int Id { get; set; }
         public string Mail { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public School School { get; set; }
+        #endregion
 
+        #region Constructors
         public Secretary()
         {
 
@@ -24,10 +27,13 @@ namespace RazorPageApplication.Models
             PhoneNumber = phoneNumber;
             School = school;
         }
+        #endregion
 
+        #region Methods
         public override string ToString()
         {
             return $"Secretary:\n\tId: {Id}\n\tMail: {Mail}\n\tName: {Name}\n\tPhonenumber: {PhoneNumber}\n\tSchool: {School}";
-        }
+        } 
+        #endregion
     }
 }
