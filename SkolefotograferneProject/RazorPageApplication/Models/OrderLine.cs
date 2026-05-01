3e7118c8902c02f5d95142c7eb0494cb425598dd
KasperKronmannMoeller <kam002@edu.zealand.dk>
2026-05-01 12:49:12 +0200

    Added region in OderLine class

--- SkolefotograferneProject/RazorPageApplication/Models/OrderLine.cs
+++ SkolefotograferneProject/RazorPageApplication/Models/OrderLine.cs
@@ -4,22 +4,25 @@ namespace RazorPageApplication.Models
 {
     public class OrderLine
     {
+        #region Properties
         public int Id { get; set; }
         public PhotoType PhotoType { get; set; }
         public PhotoColorFormat PhotoColorFormat { get; set; }
         public PhotoDimensions PhotoDimensions { get; set; }
         public double Price { get; set; }
         public int Amount { get; set; }
-        
+
 
         public double TotalPrice
         {
             get { return Price * Amount; }
-        }
+        } 
+        #endregion
 
+        #region Constructors
         public OrderLine()
         {
-            
+
         }
 
         public OrderLine(int id, PhotoType photoType, PhotoColorFormat photoColorFormat, PhotoDimensions photoDimensions, double price, int amount)
@@ -29,12 +32,15 @@ namespace RazorPageApplication.Models
             PhotoDimensions = photoDimensions;
             Price = price;
             Amount = amount;
-        }
+        } 
+        #endregion
 
+        #region Methods
         public override string ToString()
         {
-            return $"Order line: \n\tID: {Id}\n\tPhoto type: {PhotoType}\n\tPhoto Color Format: {PhotoColorFormat}\n\t" +
+            return $"Orderline: \n\tID: {Id}\n\tPhoto type: {PhotoType}\n\tPhoto Color Format: {PhotoColorFormat}\n\t" +
                 $"Photo Dimensions: {PhotoDimensions}\n\tPrice: {Price}\n\tAmount: {Amount}";
-        }
+        } 
+        #endregion
     }
 }
