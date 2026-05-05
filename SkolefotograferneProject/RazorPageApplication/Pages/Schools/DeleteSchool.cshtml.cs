acd4c79993f3256dd000c0e1c3186f1d2d19ef29
SinanCPH <cph-sf221@cphbusiness.dk>
2026-05-05 12:37:30 +0200

    Delete Frontend og backend

---
+++ SkolefotograferneProject/RazorPageApplication/Pages/Schools/DeleteSchool.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPageApplication.Interfaces;
+using RazorPageApplication.Models;
+
+namespace RazorPageApplication.Pages.Schools
+{
+    public class DeleteSchoolModel : PageModel
+    {
+        private readonly ISchoolRepositoryAsync _repo;
+        public School DeleteSchool { get; set; }
+        public DeleteSchoolModel (ISchoolRepositoryAsync repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> OnGet(int id)
+        {
+            DeleteSchool = await _repo.GetSchoolAsync(id);
+            return Page();
+        }
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            await _repo.DeleteSchoolAsync(DeleteSchool);
+            return RedirectToPage("Index");
+        }
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Index");
+        }
+    }
+}
