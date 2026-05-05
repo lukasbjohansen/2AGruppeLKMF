74f8917473c129b2bfc3c6be02ce74e772188648
Maria Litske Strauss <miiaam@live.dk>
2026-05-05 09:59:37 +0200

    Implemented GetAllSchools async and index page for Schools

--- SkolefotograferneProject/RazorPageApplication/Pages/Schools/Index.cshtml.cs
+++ SkolefotograferneProject/RazorPageApplication/Pages/Schools/Index.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPageApplication.Interfaces;
+using RazorPageApplication.Models;
 
 namespace RazorPageApplication.Pages.Schools
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private ISchoolRepositoryAsync _repo;
+        public List<School> Schools { get; set; }
+
+        public IndexModel(ISchoolRepositoryAsync schoolRepository)
         {
+            _repo = schoolRepository;
+        }
+        public async Task OnGet()
+        {
+            Schools = await _repo.GetAllSchoolsAsync();
         }
     }
 }
--- SkolefotograferneProject/RazorPageApplication/Services/SchoolRepositoryAsync.cs
+++ SkolefotograferneProject/RazorPageApplication/Services/SchoolRepositoryAsync.cs
@@ -3,6 +3,7 @@ using RazorPageApplication.Exceptions;
 using RazorPageApplication.Helpers;
 using RazorPageApplication.Interfaces;
 using RazorPageApplication.Models;
+using System.Data;
 
 namespace RazorPageApplication.Services
 {
@@ -49,12 +50,39 @@ namespace RazorPageApplication.Services
 
         public async Task<List<School>> GetAllSchoolsAsync()
         {
-            throw new NotImplementedException();
-            //string query = "SELECT * FROM School";
-            //using (SqlConnection connection = new SqlConnection(Secret.ConnectionString))
-            //{
-
-            //}
+            string query = "SELECT * FROM School";
+            List<School> schools = new List<School>();
+            using (SqlConnection connection = new SqlConnection(Secret.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        int schoolId = reader.GetInt32("SchoolID");
+                        string schoolName = reader.GetString("SchoolName");
+                        string schoolAddress = reader.GetString("SchoolAddress");
+                        string postalCode = reader.GetString("PostalCode");
+                        School school = new School(schoolId, schoolName, schoolAddress, postalCode);
+                        schools.Add(school);
+                    }
+                    reader.CloseAsync();
+                }
+                catch (SqlException sEx)
+                {
+                    sEx.PrintWithType();
+                    throw new RepositoryException(RepositoryExceptionType.Read, "Kan ikke indlæses");
+                }
+                catch (Exception ex)
+                {
+                    //ExceptionHelpers.PrintWithType(ex);
+                    ex.PrintWithType();
+                    throw new RepositoryException(RepositoryExceptionType.Read, ex.GetFullMessage());
+                }
+                return schools;
+            }
         }
 
         public async Task UpdateSchoolAsync(School school)
