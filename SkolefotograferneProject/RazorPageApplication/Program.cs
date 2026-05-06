5042655511279767c1a764a5811e0d4be8d9cfe7
fredhkris <frede.h.k1@gmail.com>
2026-05-06 11:21:52 +0200

    Execute AddTransient on other repos

--- SkolefotograferneProject/RazorPageApplication/Program.cs
+++ SkolefotograferneProject/RazorPageApplication/Program.cs
@@ -7,14 +7,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddTransient<IRepository<School>, SchoolRepositoryAsync>();
+builder.Services.AddTransient<IRepository<SchoolClass>, SchoolClassRepositoryAsync>();
+builder.Services.AddTransient<IRepository<Teacher>, TeacherRepositoryAsync>();
+builder.Services.AddTransient<IRepository<Secretary>, SecretaryRepositoryAsync>();
+builder.Services.AddTransient<IRepository<Parent>, ParentRepositoryAsync>();
+builder.Services.AddTransient<IRepository<Student>, StudentRepositoryAsync>();
+builder.Services.AddTransient<IRepository<Photographer>, PhotographerRepositoryAsync>();
+builder.Services.AddTransient<IRepository<Photo>, PhotoRepositoryAsync>();
+builder.Services.AddTransient<IRepository<PhotoEvent>, PhotoEventRepositoryAsync>();
+builder.Services.AddTransient<IRepository<Order>, OrderRepositoryAsync>();
+builder.Services.AddTransient<IRepository<OrderLine>, OrderLineRepositoryAsync>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
