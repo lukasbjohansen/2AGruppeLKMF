6140063e02b87b451653299549b158f4ea514f16
lukasbjohansen <lukasbjohansen@gmail.com>
2026-05-06 18:13:15 +0200

    made GetAsync return T? in IRepository

--- SkolefotograferneProject/RazorPageApplication/Interfaces/IRepositoryAsync.cs
+++ SkolefotograferneProject/RazorPageApplication/Interfaces/IRepositoryAsync.cs
@@ -5,7 +5,7 @@
         Task CreateAsync(T item);
         Task UpdateAsync(T item);
         Task DeleteAsync(T item);
-        Task<T> GetAsync(int id);
+        Task<T?> GetAsync(int id);
         Task<List<T>> FilterAsync(string filterCriteria);
         Task<List<T>> GetAllAsync();
     }
