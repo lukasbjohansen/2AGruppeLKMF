f596bed1269e15e886dfc270a4fb3a22d0e4cbd3
Maria Litske Strauss <miiaam@live.dk>
2026-05-07 16:50:44 +0200

    Implemented filter function in frontend for parents and started implementing PhotoRepositoryAsync

--- SkolefotograferneProject/RazorPageApplication/Pages/Parents/Index.cshtml.cs
+++ SkolefotograferneProject/RazorPageApplication/Pages/Parents/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPageApplication.Interfaces;
 using RazorPageApplication.Models;
+using System.Reflection;
 
 namespace RazorPageApplication.Pages.Parents
 {
@@ -10,13 +11,30 @@ namespace RazorPageApplication.Pages.Parents
         private IRepositoryAsync<Parent> _repo;
 
         public List<Parent> Parents { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string FilterCriteria { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string FilterBy { get; set; }
+
         public IndexModel(IRepositoryAsync<Parent> parentRepository)
         {
             _repo = parentRepository;
         }
-        public async Task OnGet()
+        //public async Task OnGet()
+        //{
+        //    Parents = await _repo.GetAllAsync();
+        //}
+
+        public async Task OnGet() //Vi vil fylde vores Members op, til det bruger vi vores metode GetAllMembers fra vores SailClubLibrary
         {
-            Parents = await _repo.GetAllAsync();
+            if (!string.IsNullOrEmpty(FilterCriteria))
+            {
+                Parents = await _repo.FilterAsync(FilterCriteria);
+            }
+            else
+                Parents = await _repo.GetAllAsync();
         }
     }
 }
--- SkolefotograferneProject/RazorPageApplication/Services/PhotoRepositoryAsync.cs
+++ SkolefotograferneProject/RazorPageApplication/Services/PhotoRepositoryAsync.cs
@@ -1,14 +1,51 @@
-﻿using RazorPageApplication.Interfaces;
+﻿using Microsoft.Data.SqlClient;
+using RazorPageApplication.Exceptions;
+using RazorPageApplication.Helpers;
+using RazorPageApplication.Interfaces;
 using RazorPageApplication.Models;
+using System.Data;
 
 namespace RazorPageApplication.Services
 {
     public class PhotoRepositoryAsync : IRepositoryAsync<Photo>
 	{
-		public Task CreateAsync(Photo item)
+        private IRepositoryAsync<Photographer> _photographerRepo;
+        private IRepositoryAsync<Student> _studentRepo;
+        public PhotoRepositoryAsync(IRepositoryAsync<Photographer> photographerRepository, IRepositoryAsync<Student> studentRepository)
+        {
+            _photographerRepo = photographerRepository;
+            _studentRepo = studentRepository;
+        }
+        public async Task CreateAsync(Photo item)
 		{
-			throw new NotImplementedException();
-		}
+            string query = "INSERT INTO Photo(PhotoID,FilePath,PhotoDate,PhotographerID,StudentID) Values(@PhotoID,@FilePath,@PhotoDate,@PhotographerID,@StudentID)";
+            await using (SqlConnection connection = new SqlConnection(Secret.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@PhotoID", item.Id);
+                    command.Parameters.AddWithValue("@FilePath", item.FilePath);
+                    command.Parameters.AddWithValue("@PhotoDate", item.Date);
+                    command.Parameters.AddWithValue("@PhotographerID", item.Photographer.Name);
+                    command.Parameters.AddWithValue("@StudentID", item.Student.Name);
+                    await command.ExecuteNonQueryAsync();
+                }
+                catch (SqlException sEx)
+                {
+                    sEx.PrintWithType();
+                    throw new RepositoryException(RepositoryExceptionType.Create, "Ugyldigt input");
+                }
+                catch (Exception ex)
+                {
+                    //ExceptionHelpers.PrintWithType(ex);
+                    ex.PrintWithType();
+                    throw new RepositoryException(RepositoryExceptionType.Create, ex.GetFullMessage());
+                }
+
+            }
+        }
 
 		public Task DeleteAsync(Photo item)
 		{
@@ -25,10 +62,43 @@ namespace RazorPageApplication.Services
             throw new NotImplementedException();
         }
 
-        public Task<List<Photo>> GetAllAsync()
+        public async Task<List<Photo>> GetAllAsync()
 		{
-			throw new NotImplementedException();
-		}
+            string query = "SELECT * FROM Photo";
+            List<Photo> photos = new List<Photo>();
+            using (SqlConnection connection = new SqlConnection(Secret.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        int photoId = reader.GetInt32("PhotoID");
+                        string filePath = reader.GetString("FilePath");
+                        DateTime date = reader.GetDateTime("PhotoDate");
+                        int photographerId = reader.GetInt32("PhotographerID");
+                        int studentId = reader.GetInt32("StudentID");
+                        Photo photo = new Photo(photoId, filePath, date);
+                        photos.Add(photo);
+                    }
+                    reader.CloseAsync();
+                }
+                catch (SqlException sEx)
+                {
+                    sEx.PrintWithType();
+                    throw new RepositoryException(RepositoryExceptionType.Read, "Kan ikke indlæses");
+                }
+                catch (Exception ex)
+                {
+                    //ExceptionHelpers.PrintWithType(ex);
+                    ex.PrintWithType();
+                    throw new RepositoryException(RepositoryExceptionType.Read, ex.GetFullMessage());
+                }
+                return photos;
+            }
+        }
 
 		public Task UpdateAsync(Photo item)
 		{
