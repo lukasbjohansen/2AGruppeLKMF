018187ce0e19d90536dbaabd0d17e51afdd57482
Frederik Kristensen <frede.h.k1@gmail.com>
2026-05-11 10:37:13 +0200

    Add neccesary repo references to CreatePhotoEvent

--- SkolefotograferneProject/RazorPageApplication/Pages/PhotoEvents/CreatePhotoEvent.cshtml.cs
+++ SkolefotograferneProject/RazorPageApplication/Pages/PhotoEvents/CreatePhotoEvent.cshtml.cs
@@ -2,17 +2,30 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPageApplication.Interfaces;
 using RazorPageApplication.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace RazorPageApplication.Pages.PhotoEvents
 {
     public class CreatePhotoEventModel : PageModel
     {
         private readonly IRepositoryAsync<PhotoEvent> _repo;
+        private readonly IRepositoryAsync<Photographer> _photographerRepo;
+        private readonly IRepositoryAsync<SchoolClass> _schoolClassRepo;
 
         [BindProperty]
         public PhotoEvent NewPhotoEvent { get; set; }
 
-        public CreatePhotoEventModel(IRepositoryAsync<PhotoEvent> repo)
+        [BindProperty]
+        [Required]
+        public int PhotographerId { get; set; }
+
+        [BindProperty]
+        [Required]
+        public int SchoolClassId { get; set; }
+
+        public CreatePhotoEventModel(IRepositoryAsync<PhotoEvent> repo,
+                                     IRepositoryAsync<Photographer> photographerRepo,
+                                     IRepositoryAsync<SchoolClass> schoolClassRepo)
         {
             _repo = repo;
         }
@@ -28,6 +41,8 @@ namespace RazorPageApplication.Pages.PhotoEvents
             {
                 return Page();
             }
+            NewPhotoEvent.Photographer = await _photographerRepo.GetAsync(PhotographerId);
+            NewPhotoEvent.SchoolClass = await _schoolClassRepo.GetAsync(SchoolClassId);
             try
             {
                 await _repo.CreateAsync(NewPhotoEvent);
