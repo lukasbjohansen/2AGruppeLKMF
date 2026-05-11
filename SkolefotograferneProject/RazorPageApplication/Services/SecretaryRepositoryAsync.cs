f892cad2b558f8514ac7bae36e903c76792e90d9
SinanCPH <cph-sf221@cphbusiness.dk>
2026-05-11 09:13:18 +0200

    Secretary starten på CRUD

--- SkolefotograferneProject/RazorPageApplication/Services/SecretaryRepositoryAsync.cs
+++ SkolefotograferneProject/RazorPageApplication/Services/SecretaryRepositoryAsync.cs
@@ -1,4 +1,7 @@
-﻿using RazorPageApplication.Interfaces;
+﻿using Microsoft.Data.SqlClient;
+using RazorPageApplication.Exceptions;
+using RazorPageApplication.Helpers;
+using RazorPageApplication.Interfaces;
 using RazorPageApplication.Models;
 
 namespace RazorPageApplication.Services
@@ -10,32 +13,43 @@ namespace RazorPageApplication.Services
             
         }
 
-        public Task CreateAsync(Secretary item)
+        //Create, GetAll, Get, Delete, Update, Filter
+        public async Task CreateAsync(Secretary item)
         {
-            throw new NotImplementedException();
+            string query = "INSERT INTO Secretary(SecretaryName, PhoneNumber, Mail, SecretaryPassword, SchoolID) Values (@SecretaryName, @PhoneNumber, @Mail, @SecretaryPassword, @SchoolID)";
+            await using (SqlConnection connection = new SqlConnection(Secret.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@SecretaryName", item.Name);
+                    command.Parameters.AddWithValue("@PhoneNumber", item.PhoneNumber);
+                    command.Parameters.AddWithValue("@Mail", item.Mail);
+                    command.Parameters.AddWithValue("@SecretaryPassword", item.Password);
+                    command.Parameters.AddWithValue("@SchoolID", item.School); //kan vel kun være School
+                    await command.ExecuteNonQueryAsync();
+                }
+                catch (SqlException sEx)
+                {
+                    sEx.PrintWithType();
+                    throw new RepositoryException(RepositoryExceptionType.Create, "Ugyldigt input");
+                }
+                catch (Exception ex)
+                {
+                    //ExceptionHelpers.PrintWithType(ex);
+                    ex.PrintWithType();
+                    throw new RepositoryException(RepositoryExceptionType.Create, ex.GetFullMessage());
+                }
+            }   
         }
 
-        public async Task CreateUserAsync(Secretary user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task DeleteAsync(Secretary item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task DeleteUserAsync(Secretary user)
+        public Task DeleteAsync(Secretary item) //DEN HAR EN FK SÅ KAN IKKE SLETTE EN SECRETARY
         {
             throw new NotImplementedException();
         }
 
         public Task<List<Secretary>> FilterAsync(string filterCriteria)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Secretary>> FilterUserAsync(string filterCriteria)
         {
             throw new NotImplementedException();
         }
@@ -50,17 +64,7 @@ namespace RazorPageApplication.Services
             throw new NotImplementedException();
         }
 
-        public async Task<List<Secretary>> GetAllUsersAsync()
-        {
-            throw new NotImplementedException();
-        }
-
         public Task UpdateAsync(Secretary item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task UpdateUserAsync(Secretary user)
         {
             throw new NotImplementedException();
         }
