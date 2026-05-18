d77604001bc188f5e38690c087368f9f41d01439
Frederik Kristensen <frede.h.k1@gmail.com>
2026-05-18 09:32:11 +0200

    Add GenericComparer

---
+++ SkolefotograferneProject/RazorPageApplication/Helpers/Sorting/GenericComparer.cs
@@ -0,0 +1,32 @@
+﻿namespace RazorPageApplication.Helpers.Sorting
+{
+    public class GenericComparer<T, TKey> : IComparer<T> where TKey : IComparable
+    {
+        private readonly Func<T, TKey> _selector;
+        private readonly bool _descending;
+
+        public GenericComparer(Func<T, TKey> selector, bool descending = false)
+        {
+            _selector = selector;
+            _descending = descending;
+        }
+
+        public int Compare(T? x, T? y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+            if (x == null)
+            {
+                return -1;
+            }
+            if (y == null)
+            {
+                return 1;
+            }
+            int res = _selector(x).CompareTo(_selector(y));
+            return _descending ? -res : res;
+        }
+    }
+}
