1d8da5a1fe0779b35388d7f9c71fb30dd8b768eb
Maria Litske Strauss <miiaam@live.dk>
2026-05-18 10:44:34 +0200

    Added filter function to photos

--- SkolefotograferneProject/RazorPageApplication/Pages/Photos/Index.cshtml.cs
+++ SkolefotograferneProject/RazorPageApplication/Pages/Photos/Index.cshtml.cs
@@ -10,13 +10,66 @@ namespace RazorPageApplication.Pages.Photos
         private IRepositoryAsync<Photo> _repo;
 
         public List<Photo> Photos { get; set; }
+
+
+        [BindProperty(SupportsGet = true)]
+        public string FilterCriteria { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string FilterBy { get; set; }
         public IndexModel(IRepositoryAsync<Photo> photoRepository)
         {
             _repo = photoRepository;
         }
         public async Task OnGet()
         {
-            Photos = await _repo.GetAllAsync();
+            var allPhotos = await _repo.GetAllAsync();
+
+            if (!string.IsNullOrEmpty(FilterCriteria))
+            {
+                switch (FilterBy)
+                {
+
+                    case "FilePath":
+                        Photos = allPhotos
+                            .Where(sc => sc.FilePath.Contains(FilterCriteria, StringComparison.OrdinalIgnoreCase))
+                            .ToList();
+                        break;
+
+                    case "Mail":
+                        Photos = allPhotos
+                            .Where(sc => sc.Date.ToString().Contains(FilterCriteria))
+                            .ToList();
+                        break;
+
+                    case "PhotographerID":
+                        Photos = allPhotos
+                            .Where(sc => sc != null && sc.Photographer.Name.Contains(FilterCriteria, StringComparison.OrdinalIgnoreCase))
+                            .ToList();
+                        break;
+
+                    case "StudentID":
+                        Photos = allPhotos
+                            .Where(sc => sc != null && sc.Student.Name.Contains(FilterCriteria, StringComparison.OrdinalIgnoreCase))
+                            .ToList();
+                        break;
+
+                    case "All":
+                        Photos = allPhotos
+                            .Where(sc =>
+                                sc.FilePath.Contains(FilterCriteria, StringComparison.OrdinalIgnoreCase) ||
+                                sc.Date.ToString().Contains(FilterCriteria) ||
+                                sc.Photographer.Name.Contains(FilterCriteria, StringComparison.OrdinalIgnoreCase) ||
+                                sc.Student.Name.Contains(FilterCriteria, StringComparison.OrdinalIgnoreCase))
+                            .ToList();
+                        break;
+                }
+
+            }
+            else
+            {
+                Photos = allPhotos;
+            }
         }
     }
 }
