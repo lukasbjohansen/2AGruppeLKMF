46f25f3c916369bd8e29ef15d2cafea3a99d6150
lukasbjohansen <lukasbjohansen@gmail.com>
2026-05-20 13:44:31 +0200

    test foundation made

---
+++ SkolefotograferneProject/TestProject1/TeacherRepositoryTest.cs
@@ -0,0 +1,35 @@
+﻿using RazorPageApplication.Enums;
+using RazorPageApplication.Models;
+using RazorPageApplication.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    [TestClass]
+    [DoNotParallelize]
+    public sealed class TeacherRepositoryTest
+    {
+        private TeacherRepositoryAsync _repository;
+        private readonly List<Teacher> _createdTeachers = new();
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _repository = new TeacherRepositoryAsync();
+            _createdTeachers.Clear();
+        }
+        [TestCleanup]
+        public async Task Cleanup()
+        {
+            foreach (Teacher teacher in _createdTeachers)
+            {
+                await _repository.DeleteAsync(teacher);
+            }
+            _createdTeachers.Clear();
+        }
+    }
+}
