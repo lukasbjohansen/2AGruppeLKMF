ce595a017d94db4fa095079c332f3c3981d1f151
Maria Litske Strauss <miiaam@live.dk>
2026-05-20 11:13:50 +0200

    Fixed placeholder image

--- SkolefotograferneProject/RazorPageApplication/Exceptions/RepositoryException.cs
+++ SkolefotograferneProject/RazorPageApplication/Exceptions/RepositoryException.cs
@@ -5,14 +5,13 @@ namespace RazorPageApplication.Exceptions
     public class RepositoryException : Exception
     {
         public RepositoryExceptionType Type { get; }
+        public override string Message { get { return base.Message; } }
+        public string FullMessage { get { return $"Repository exception ({Type}): {this.GetFullMessage()}"; } }
 
         public RepositoryException(RepositoryExceptionType type, string message) : base (message)
         {
             Type = type;
         }
-
-        public override string Message { get { return base.Message; } }
-        public string FullMessage { get { return $"Repository exception ({Type}): {this.GetFullMessage()}"; } }
     }
 
     public enum RepositoryExceptionType
