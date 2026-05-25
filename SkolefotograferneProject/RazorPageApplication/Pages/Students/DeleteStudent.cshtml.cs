e22699b9c23a59f1978dbe10431379eb1f733fb7
KasperKronmannMoeller <kam002@edu.zealand.dk>
2026-05-25 20:37:59 +0200

    Changed some Summaries  for Student/SchoolClass Service class and Student Pages

--- SkolefotograferneProject/RazorPageApplication/Pages/Students/DeleteStudent.cshtml.cs
+++ SkolefotograferneProject/RazorPageApplication/Pages/Students/DeleteStudent.cshtml.cs
@@ -8,33 +8,54 @@ namespace RazorPageApplication.Pages.Students
     public class DeleteStudentModel : PageModel
     {
         #region Instance fields
-        private IRepositoryAsync<Student> _repo;
+        /// <summary>
+        /// _studentRepo instance field that represents the repository for the Student model. This field is used to access the methods of the repository to retrieve and manipulate student data from the database when the delete student page is accessed.
+        /// </summary>
+        private IRepositoryAsync<Student> _studentRepo;
         #endregion
 
         #region Properties
+        /// <summary>
+        /// DeleteStudent property that represents the student that is being deleted on the delete student page. This property is used to display the details of the student that is being deleted on the user interface and to pass the student
+        /// </summary>
         public Student DeleteStudent { get; set; }
         #endregion
 
         #region Constructors
-        public DeleteStudentModel(IRepositoryAsync<Student> repo)
+        /// <summary>
+        /// DeleteStudentModel constructor that takes an IRepositoryAsync<Student> parameter and assigns it to the _studentRepo instance field. This allows the DeleteStudentModel class to access the methods of the repository to retrieve and manipulate student data from the database when the delete student page is accessed.
+        /// </summary>
+        public DeleteStudentModel(IRepositoryAsync<Student> studentRepo)
         {
-            _repo = repo;
+            _studentRepo = studentRepo;
 
         }
         #endregion
 
         #region Methods
+        /// <summary>
+        /// Onget method that takes an int id parameter and retrieves the student with the specified id from the database using the GetAsync method of the _studentRepo repository. The retrieved student is then assigned to the DeleteStudent property, which is used to display the details of the student on the delete student page. Finally, the method returns the Page() method to render the delete student page with the retrieved student details.
+        /// </summary>
         public async Task<IActionResult> OnGet(int id)
         {
-            DeleteStudent = await _repo.GetAsync(id);
+            DeleteStudent = await _studentRepo.GetAsync(id);
             return Page();
         }
+
+        /// <summary>
+        /// OnPostDelete method that takes an int id parameter and retrieves the student with the specified id from the database using the GetAsync method of the _studentRepo repository. The retrieved student is then assigned to the DeleteStudent property, which is used to delete the student from the database using the DeleteAsync method of the _studentRepo repository. Finally, the method redirects the user to the index page of the students section using the RedirectToPage() method.
+        /// </summary>
         public async Task<IActionResult> OnPostDelete(int id)
         {
-            DeleteStudent = await _repo.GetAsync(id);
-            await _repo.DeleteAsync(DeleteStudent);
+            DeleteStudent = await _studentRepo.GetAsync(id);
+            await _studentRepo.DeleteAsync(DeleteStudent);
             return RedirectToPage("Index");
         }
+
+        /// <summary>
+        /// OnPost method that is called when the user clicks the cancel button on the delete student page. This method simply redirects the user back to the index page of the students section using the RedirectToPage() method, without performing any deletion of student data from the database.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult OnPost()
         {
             return RedirectToPage("Index");
--- SkolefotograferneProject/RazorPageApplication/Pages/Students/EditStudent.cshtml.cs
+++ SkolefotograferneProject/RazorPageApplication/Pages/Students/EditStudent.cshtml.cs
@@ -11,44 +11,76 @@ namespace RazorPageApplication.Pages.Students
     public class EditStudentModel : PageModel
     {
         #region Instance fields
-        private readonly IRepositoryAsync<Student> _repo;
+        /// <summary>
+        /// _repo instance field that represents the repository for the Student model.
+        /// </summary>
+        private readonly IRepositoryAsync<Student> _studentRepo;
+
+        /// <summary>
+        /// _parentRepo instance field that represents the repository for the Parent model.
+        /// </summary>
         private readonly IRepositoryAsync<Parent> _parentRepo;
+
+        /// <summary>
+        /// _schoolClassRepo instance field that represents the repository for the SchoolClass model.
+        /// </summary>
         private readonly IRepositoryAsync<SchoolClass> _schoolClassRepo;
 
         #endregion
 
         #region Properties
+        /// <summary>
+        /// StudentToUpdate property that represents the student that is being edited on the edit student page.
+        /// </summary>
         [BindProperty]
         public Student StudentToUpdate { get; set; }
 
+        /// <summary>
+        /// ParentId property that represents the id of the parent that is selected in the dropdown menu on the edit student page.
+        /// </summary>
         [BindProperty]
         [Required]
         public string ParentId { get; set; }
 
+        /// <summary>
+        /// SchoolClassId property that represents the id of the school class that is selected in the dropdown menu on the edit student page.
+        /// </summary>
         [BindProperty]
         [Required]
         public string SchoolClassId { get; set; }
 
+        /// <summary>
+        /// SchoolClassSelect property that represents the list of school classes that will be displayed in the dropdown menu on the edit student page.
+        /// </summary>
         [BindProperty]
         public IEnumerable<SelectListItem> SchoolClassSelect { get; set; }
 
+        /// <summary>
+        /// ParentSelect property that represents the list of parents that will be displayed in the dropdown menu on the edit student page.
+        /// </summary>
         [BindProperty]
         public IEnumerable<SelectListItem> ParentSelect { get; set; }
         #endregion
 
         #region Constructors
-        public EditStudentModel(IRepositoryAsync<Student> repo, IRepositoryAsync<Parent> teacherRepo, IRepositoryAsync<SchoolClass> schoolRepo)
+        /// <summary>
+        /// EditStudentModel constructor that is used to create a new instance of the EditStudentModel class with the repositories for the Student, Parent, and SchoolClass models injected as parameters.
+        /// </summary>
+        public EditStudentModel(IRepositoryAsync<Student> studentRepo, IRepositoryAsync<Parent> teacherRepo, IRepositoryAsync<SchoolClass> schoolRepo)
         {
-            _repo = repo;
+            _studentRepo = studentRepo;
             _parentRepo = teacherRepo;
             _schoolClassRepo = schoolRepo;
         }
         #endregion
 
         #region Methods
+        /// <summary>
+        /// OnGet method that is called when the user navigates to the edit student page.
+        /// </summary>
         public async Task OnGet(int id)
         {
-            StudentToUpdate = await _repo.GetAsync(id);
+            StudentToUpdate = await _studentRepo.GetAsync(id);
             List<SchoolClass> schoolClasses = await _schoolClassRepo.GetAllAsync();
             List<Parent> parents = await _parentRepo.GetAllAsync();
 
@@ -58,7 +90,10 @@ namespace RazorPageApplication.Pages.Students
             SchoolClassId = StudentToUpdate.SchoolClass.Id.ToString();
             ParentId = StudentToUpdate.Parent.Id.ToString();
         }
-
+        /// <summary>
+        /// OnPostUpdate method that is called when the user submits the form to update a student's information on the edit student page.
+        /// </summary>
+        /// <returns></returns>
         public async Task<IActionResult> OnPostUpdate()
         {
             if (!ModelState.IsValid)
@@ -71,7 +106,7 @@ namespace RazorPageApplication.Pages.Students
             {
                 StudentToUpdate.SchoolClass = await _schoolClassRepo.GetAsync(Convert.ToInt32(SchoolClassId));
                 StudentToUpdate.Parent = await _parentRepo.GetAsync(Convert.ToInt32(ParentId));
-                await _repo.UpdateAsync(StudentToUpdate);
+                await _studentRepo.UpdateAsync(StudentToUpdate);
                 return RedirectToPage("Index");
             }
             catch (SqlException e)
