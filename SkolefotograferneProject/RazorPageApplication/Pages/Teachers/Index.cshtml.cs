57b04ddb79f3d0094b1c45c0e6a713f4ead65fbb
lukasbjohansen <lukasbjohansen@gmail.com>
2026-05-27 10:15:48 +0200

    halfdone stuff

--- SkolefotograferneProject/RazorPageApplication/Pages/Teachers/Index.cshtml.cs
+++ SkolefotograferneProject/RazorPageApplication/Pages/Teachers/Index.cshtml.cs
@@ -1,14 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPageApplication.Enums;
+using RazorPageApplication.Helpers.Sorting;
 using RazorPageApplication.Interfaces;
 using RazorPageApplication.Models;
+using System.Globalization;
 
 namespace RazorPageApplication.Pages.Teachers
 {
     public class IndexModel : PageModel
     {
         private readonly ITeacherRepository _repo;
+        private bool _isDescending;
 
         public List<Teacher> Teachers { get; set; } = new List<Teacher>();
 
@@ -17,12 +20,15 @@ namespace RazorPageApplication.Pages.Teachers
 
         [BindProperty(SupportsGet = true)]
         public TeacherFilterBy FilterBy { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SortBy { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string LastSortBy { get; set; }
 
         public IndexModel(ITeacherRepository teacherRepository)
         {
             _repo = teacherRepository;
-            FilterCriteria = "";
-            FilterBy = TeacherFilterBy.All;
+            _isDescending = false;
         }
 
         public async Task OnGetAsync()
@@ -34,6 +40,28 @@ namespace RazorPageApplication.Pages.Teachers
             else
             {
                 Teachers = await _repo.FilterAsync(FilterCriteria, FilterBy);
+            }
+            if (!string.IsNullOrEmpty(SortBy)) SortTeachers();
+        }
+        private void SortTeachers()
+        {
+            if (LastSortBy == SortBy) _isDescending = !_isDescending;
+            else _isDescending = false;
+            LastSortBy = SortBy;
+            switch (SortBy)
+            {
+                case "Id":
+                    Teachers.Sort(new GenericComparer<Teacher, int>(p => p.Id, _isDescending));
+                    break;
+                case "Name":
+                    Teachers.Sort(new GenericComparer<Teacher, string>(p => p.Name, _isDescending));
+                    break;
+                case "Mail":
+                    Teachers.Sort(new GenericComparer<Teacher, string>(p => p.Mail, _isDescending));
+                    break;
+                case "PhoneNumber":
+                    Teachers.Sort(new GenericComparer<Teacher, string>(p => p.PhoneNumber, _isDescending));
+                    break;
             }
         }
     }
