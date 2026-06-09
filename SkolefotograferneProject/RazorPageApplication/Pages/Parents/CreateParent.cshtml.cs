b07470347a3412e6d96214b3b0fdbe6d5fb5c5ae
Maria Litske Strauss <miiaam@live.dk>
2026-06-09 08:29:58 +0200

    Added comments to CreateParent cs-page

--- SkolefotograferneProject/RazorPageApplication/Pages/Parents/CreateParent.cshtml.cs
+++ SkolefotograferneProject/RazorPageApplication/Pages/Parents/CreateParent.cshtml.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.FileSystemGlobbing;
 using RazorPageApplication.Interfaces;
 using RazorPageApplication.Models;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
 
 namespace RazorPageApplication.Pages.Parents
 {
@@ -28,21 +32,30 @@ namespace RazorPageApplication.Pages.Parents
         #region Methods
         public void OnGet()
         {
+            // new Parent sørger for, at objektet findes i hukommelsen, så formularen har et sted at 'skrive til'. Det gør at vi undgår, at parent er null.
+            // new Parent binder datab fra vores form, når brugeren trykker 'Gem' eller 'Opret'.
+            //new Parent() i OnGet sikrer, at der er en tom "beholder" klar til at tage imod den data, som brugeren indtaster i formularen.
             NewParent = new Parent();
         }
 
         public async Task<IActionResult> OnPost()
         {
+            // Vi tjekker, om de data brugeren har skrevet i formularen er valide (om de er required osv.)
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+            // Hvis valideringen var i orden, ryger vi ned i vores try-del, hvor vores injected repository (_repo) bliver kaldt asynkront for at oprette et nyt objekt (NewParent) i databasen.
+            // Hvis det lykkes, sendes brugeren videre til Index-siden
             try
             {
                 await _repo.CreateAsync(NewParent);
                 return RedirectToPage("Index");
             }
-
+            // Vi ryger ned i catch-delen, hvis noget går galt i vores try.
+            // Fejlbeskeden gemmes i ViewData["ErrorMessage"]
+            // Fejlen tilføjes til ModelState, hvilket gør, at den kan vises på vores foranliggende side.
+            // Til sidst genindlæses siden, så brugeren får besked om, hvad der gik galt.
             catch (Exception ex)
             {
                 ViewData["ErrorMessage"] = ex.Message;
